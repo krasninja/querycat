bdcda9678217e07af6ce1e720189645d09dee20c
Ivan Kozhin <ivan@saritasa.com>
2023-01-15 22:22:29 +0700

    Add MacOS and Linux ARM targets

---
+++ src/QueryCat.Build/Tasks/BuildMacTask.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Cake.Common.Tools.DotNet;
+using Cake.Frosting;
+
+namespace QueryCat.Build.Tasks;
+
+[TaskName("Build-Mac")]
+[TaskDescription("Build project for Mac target")]
+public sealed class BuildMacTask : AsyncFrostingTask<BuildContext>
+{
+    /// <inheritdoc />
+    public override Task RunAsync(BuildContext context)
+    {
+        context.DotNetPublish(context.ConsoleAppProjectDirectory, new PublishGeneralSettings(context)
+        {
+            OutputDirectory = context.OutputDirectory,
+            Runtime = DotNetConstants.RidMacOSXArm64,
+        });
+        context.DotNetPublish(context.TimeItAppProjectDirectory, new PublishGeneralSettings(context)
+        {
+            OutputDirectory = context.OutputDirectory,
+            Runtime = DotNetConstants.RidMacOSXArm64,
+        });
+        return Task.CompletedTask;
+    }
+}
