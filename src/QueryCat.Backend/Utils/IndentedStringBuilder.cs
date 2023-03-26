485df2a90814627047f587310aa0021e26377ea9
Ivan Kozhin <ivan@saritasa.com>
2023-03-26 19:59:10 +0700

    Initial implementation of GraphQLQueryBuilder

--- src/QueryCat.Backend/Utils/IndentedStringBuilder.cs
+++ src/QueryCat.Backend/Utils/IndentedStringBuilder.cs
@@ -11,14 +11,27 @@ public class IndentedStringBuilder
     private readonly int _indentSize;
     private readonly StringBuilder _stringBuilder = new();
     private int _indent;
+    private bool _isEmpty; // The marker to indicate that there were no output in the current line.
+
+    /// <summary>
+    /// Indent size.
+    /// </summary>
+    public int IndentSize => _indentSize;
+
+    /// <summary>
+    /// Current indent level.
+    /// </summary>
+    public int Indent => _indent;
 
     /// <summary>
     /// Constructor.
     /// </summary>
     /// <param name="indentSize">Indent size.</param>
-    public IndentedStringBuilder(int indentSize = 2)
+    /// <param name="skipFirstLineIndent">Skip indent for the first line.</param>
+    public IndentedStringBuilder(int indentSize = 2, bool skipFirstLineIndent = false)
     {
         _indentSize = indentSize;
+        _isEmpty = !skipFirstLineIndent;
     }
 
     /// <summary>
@@ -27,12 +40,23 @@ public class IndentedStringBuilder
     /// <param name="value">String value.</param>
     public IndentedStringBuilder AppendLine(string value)
     {
-        var lines = value.Split(Environment.NewLine);
-        foreach (var line in lines)
+        if (!value.Contains(Environment.NewLine))
         {
             AppendIndent();
-            _stringBuilder.AppendLine(line);
+            _stringBuilder.AppendLine(value);
         }
+        else
+        {
+            var lines = value.Split(Environment.NewLine);
+            foreach (var line in lines)
+            {
+                AppendIndent();
+                _isEmpty = true;
+                _stringBuilder.AppendLine(line);
+            }
+        }
+
+        _isEmpty = !value.EndsWith(Environment.NewLine);
         return this;
     }
 
@@ -42,8 +66,129 @@ public class IndentedStringBuilder
     public IndentedStringBuilder AppendLine()
     {
         _stringBuilder.AppendLine();
+        _isEmpty = true;
         return this;
     }
+
+    #region Overloads
+
+    public IndentedStringBuilder Append(string value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        if (value == Environment.NewLine)
+        {
+            _isEmpty = true;
+        }
+        return this;
+    }
+
+    public IndentedStringBuilder Append(IndentedStringBuilder stringBuilder)
+    {
+        AppendIndent();
+        _stringBuilder.Append(stringBuilder._stringBuilder);
+        if (stringBuilder._stringBuilder[^1] == '\n')
+        {
+            _isEmpty = true;
+        }
+        return this;
+    }
+
+    public IndentedStringBuilder Append(StringBuilder stringBuilder)
+    {
+        AppendIndent();
+        _stringBuilder.Append(stringBuilder);
+        if (stringBuilder[^1] == '\n')
+        {
+            _isEmpty = true;
+        }
+        return this;
+    }
+
+    public IndentedStringBuilder Append(sbyte value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(byte value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(short value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(int value)
+    {
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(long value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(float value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(double value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(decimal value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(ushort value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(char value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(uint value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    public IndentedStringBuilder Append(ulong value)
+    {
+        AppendIndent();
+        _stringBuilder.Append(value);
+        return this;
+    }
+
+    #endregion
 
     /// <summary>
     /// Increment indent.
@@ -51,6 +196,15 @@ public class IndentedStringBuilder
     public IndentedStringBuilder IncreaseIndent()
     {
         _indent++;
+        return this;
+    }
+
+    /// <summary>
+    /// Increment indent.
+    /// </summary>
+    public IndentedStringBuilder IncreaseIndent(int level)
+    {
+        _indent += level * _indentSize;
         return this;
     }
 
@@ -69,7 +223,11 @@ public class IndentedStringBuilder
 
     private void AppendIndent()
     {
-        _stringBuilder.Append(' ', _indent * _indentSize);
+        if (_isEmpty)
+        {
+            _stringBuilder.Append(' ', _indent * _indentSize);
+            _isEmpty = false;
+        }
     }
 
     /// <inheritdoc />
