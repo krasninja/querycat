1290c9165fb7963906afce83a9dbdde4146cb869
Ivan Kozhin <ivan@saritasa.com>
2023-06-13 09:05:43 +0700

    Remove unused namespaces

--- src/QueryCat.Build/Tasks/CleanTask.cs
+++ src/QueryCat.Build/Tasks/CleanTask.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks;
 using Cake.Frosting;
 
 namespace QueryCat.Build.Tasks;
--- src/QueryCat.Build/Tasks/GetInputsInMarkdownTask.cs
+++ src/QueryCat.Build/Tasks/GetInputsInMarkdownTask.cs
@@ -1,8 +1,4 @@
-using System;
-using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Cake.Core;
 using Cake.Core.Diagnostics;
 using Cake.Frosting;
