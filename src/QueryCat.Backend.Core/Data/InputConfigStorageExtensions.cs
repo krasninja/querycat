e17fb1658dfbfbb7a534697463bfd1ab3bbd0667
Ivan Kozhin <ivan@saritasa.com>
2023-08-27 23:26:39 +0700

    Move InputConfigStorageExtensions to core

--- src/QueryCat.Backend.Core/Data/InputConfigStorageExtensions.cs
+++ src/QueryCat.Backend.Core/Data/InputConfigStorageExtensions.cs
@@ -1,7 +1,6 @@
-using QueryCat.Backend.Core.Data;
 using QueryCat.Backend.Core.Types;
 
-namespace QueryCat.Backend.Storage;
+namespace QueryCat.Backend.Core.Data;
 
 /// <summary>
 /// Extensions for <see cref="IInputConfigStorage" />.
