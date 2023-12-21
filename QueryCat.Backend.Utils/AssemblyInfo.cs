699172fef7e108ca380e3f6962f0e55c5df538de
Ivan Kozhin <ivan@saritasa.com>
2023-12-21 15:11:18 +0700

    Move Utils project to src directory

--- QueryCat.Backend.Utils/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("QueryCat.Backend"),
-           InternalsVisibleTo("QueryCat.Backend.Formatters"),
-           InternalsVisibleTo("QueryCat.Backend.Core"),
-           InternalsVisibleTo("QueryCat.UnitTests")]
