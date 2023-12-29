a496640f7e33d59b06849364eae9b38a986012b1
Ivan Kozhin <ivan@saritasa.com>
2023-12-29 19:31:54 +0700

    Use StringComparer.InvariantCultureIgnoreCase

--- src/QueryCat.Backend/Execution/ExecutionScope.cs
+++ src/QueryCat.Backend/Execution/ExecutionScope.cs
@@ -10,33 +10,11 @@ public sealed class ExecutionScope : IExecutionScope
 {
     private readonly ExecutionScope? _parent;
 
-    private sealed class CaseInsensitiveEqualityComparer : IEqualityComparer<string>
-    {
-        public static CaseInsensitiveEqualityComparer Instance { get; } = new();
-
-        /// <inheritdoc />
-        public bool Equals(string? x, string? y)
-        {
-            if (x == y)
-            {
-                return true;
-            }
-            if (x == null || y == null)
-            {
-                return false;
-            }
-            return x.Equals(y, StringComparison.OrdinalIgnoreCase);
-        }
-
-        /// <inheritdoc />
-        public int GetHashCode(string obj) => obj.ToUpper().GetHashCode();
-    }
-
     /// <summary>
     /// Variables array.
     /// </summary>
     public IDictionary<string, VariantValue> Variables { get; }
-        = new Dictionary<string, VariantValue>(CaseInsensitiveEqualityComparer.Instance);
+        = new Dictionary<string, VariantValue>(StringComparer.InvariantCultureIgnoreCase);
 
     /// <inheritdoc />
     public IExecutionScope? Parent => _parent;
