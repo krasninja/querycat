2eeb68e75da8d7c2e1939e11e38aaaec963f7398
Ivan Kozhin <ivan@saritasa.com>
2024-01-17 19:14:20 +0700

    Update clean task

--- src/QueryCat.Build/Tasks/CleanTask.cs
+++ src/QueryCat.Build/Tasks/CleanTask.cs
@@ -1,3 +1,5 @@
+using Cake.Common.Tools.DotNet;
+using Cake.Common.Tools.DotNet.Clean;
 using Cake.Frosting;
 
 namespace QueryCat.Build.Tasks;
@@ -8,6 +10,34 @@ public class CleanTask : AsyncFrostingTask<BuildContext>
     /// <inheritdoc />
     public override Task RunAsync(BuildContext context)
     {
+        // Dotnet clean with Release configuration.
+        context.DotNetClean(context.ConsoleAppProjectDirectory, new DotNetCleanSettings
+        {
+            Configuration = DotNetConstants.ConfigurationRelease,
+        });
+        context.DotNetClean(context.TimeItAppProjectDirectory, new DotNetCleanSettings
+        {
+            Configuration = DotNetConstants.ConfigurationRelease,
+        });
+        context.DotNetClean(context.TestPluginAppProjectDirectory, new DotNetCleanSettings
+        {
+            Configuration = DotNetConstants.ConfigurationRelease,
+        });
+
+        // Dotnet clean with Debug configuration.
+        context.DotNetClean(context.ConsoleAppProjectDirectory, new DotNetCleanSettings
+        {
+            Configuration = DotNetConstants.ConfigurationDebug,
+        });
+        context.DotNetClean(context.TimeItAppProjectDirectory, new DotNetCleanSettings
+        {
+            Configuration = DotNetConstants.ConfigurationDebug,
+        });
+        context.DotNetClean(context.TestPluginAppProjectDirectory, new DotNetCleanSettings
+        {
+            Configuration = DotNetConstants.ConfigurationDebug,
+        });
+
         // TODO:
         return Task.CompletedTask;
     }
