5bbcda23cddf16ba2971b0e1da0ef55ab07de9c0
Ivan Kozhin <ivan@saritasa.com>
2024-11-15 13:35:27 +0700

    Add more benchmarks

--- src/QueryCat.Benchmarks/Benchmarks/VariantValueBenchmarks.cs
+++ src/QueryCat.Benchmarks/Benchmarks/VariantValueBenchmarks.cs
@@ -65,4 +65,42 @@ public class VariantValueBenchmarks
         }
         return value;
     }
+
+    public static VariantValue SumVal(in VariantValue v1, in VariantValue v2, in VariantValue v3, in VariantValue v4, in VariantValue v5)
+        => new(v1.AsFloatUnsafe + v2.AsFloatUnsafe + v3.AsFloatUnsafe + v4.AsFloatUnsafe + v5.AsFloatUnsafe);
+
+    [Benchmark]
+    public VariantValue PassByValue()
+    {
+        var arg1 = new VariantValue(10.53);
+        var arg2 = new VariantValue(11.53);
+        var arg3 = new VariantValue(12.53);
+        var arg4 = new VariantValue(13.53);
+        var arg5 = new VariantValue(14.53);
+        var sum = new VariantValue(0.0);
+        for (var i = 0; i < 1000; i++)
+        {
+            sum += SumVal(arg1, arg2, arg3, arg4, arg5);
+        }
+        return sum;
+    }
+
+    public static VariantValue SumRef(ref VariantValue v1, ref VariantValue v2, ref VariantValue v3, ref VariantValue v4, ref VariantValue v5)
+        => new(v1.AsFloatUnsafe + v2.AsFloatUnsafe + v3.AsFloatUnsafe + v4.AsFloatUnsafe + v5.AsFloatUnsafe);
+
+    [Benchmark]
+    public VariantValue PassByRef()
+    {
+        var arg1 = new VariantValue(10.53);
+        var arg2 = new VariantValue(11.53);
+        var arg3 = new VariantValue(12.53);
+        var arg4 = new VariantValue(13.53);
+        var arg5 = new VariantValue(14.53);
+        var sum = new VariantValue(0.0);
+        for (var i = 0; i < 1000; i++)
+        {
+            sum += SumRef(ref arg1, ref arg2, ref arg3, ref arg4, ref arg5);
+        }
+        return sum;
+    }
 }
