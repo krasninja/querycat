055c80932f1b03011f14d5284662f8d91d48f4c3
Ivan Kozhin <ivan@saritasa.com>
2025-01-27 12:18:10 +0700

    Comment fix

--- src/QueryCat.Backend.Core/Functions/SafeFunctionAttribute.cs
+++ src/QueryCat.Backend.Core/Functions/SafeFunctionAttribute.cs
@@ -2,7 +2,7 @@ namespace QueryCat.Backend.Core.Functions;
 
 /// <summary>
 /// Safe function is the function with no side effects. It means that it only reads a data
-/// and does not effect system state (does not write anything).
+/// and does not affect system state (does not write anything).
 /// </summary>
 [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
 public sealed class SafeFunctionAttribute : Attribute;
