68d06ebe81cfdf6137e71fc12ce38b4f08771b5f
Ivan Kozhin <ivan@saritasa.com>
2025-03-12 13:46:19 +0700

    Add function json_array_elements

---
+++ src/QueryCat.Backend/Relational/ListRowsIterator.cs
@@ -0,0 +1,66 @@
+using QueryCat.Backend.Core.Data;
+using QueryCat.Backend.Core.Types;
+
+namespace QueryCat.Backend.Relational;
+
+internal sealed class ListRowsIterator : IRowsIterator
+{
+    private readonly IReadOnlyList<VariantValue> _values;
+    private readonly Row _currentRow;
+    private int _currentIndex = -1;
+
+    /// <inheritdoc />
+    public Column[] Columns { get; }
+
+    /// <inheritdoc />
+    public Row Current => _currentRow;
+
+    public ListRowsIterator(IReadOnlyList<VariantValue> values, DataType? dataType = null)
+    {
+        _values = values;
+
+        // Determine type.
+        if (dataType == null)
+        {
+            dataType = DataType.String;
+            foreach (var value in _values)
+            {
+                if (!value.IsNull)
+                {
+                    dataType = value.Type;
+                    break;
+                }
+            }
+        }
+
+
+        // Determine type.
+        Columns =
+        [
+            new Column("value", dataType.Value),
+        ];
+        _currentRow = new Row(Columns);
+    }
+
+    /// <inheritdoc />
+    public ValueTask<bool> MoveNextAsync(CancellationToken cancellationToken = default)
+    {
+        _currentIndex++;
+        var isInRange = _currentIndex > -1 && _currentIndex < _values.Count;
+        _currentRow[0] = isInRange ? _values[_currentIndex] : VariantValue.Null;
+        return ValueTask.FromResult(isInRange);
+    }
+
+    /// <inheritdoc />
+    public Task ResetAsync(CancellationToken cancellationToken = default)
+    {
+        _currentIndex = -1;
+        _currentRow[0] = VariantValue.Null;
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    public void Explain(IndentedStringBuilder stringBuilder)
+    {
+    }
+}
