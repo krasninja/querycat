6bd3126d83f56d17701e8e2694f0ff35a5c8441f
Ivan Kozhin <ivan@saritasa.com>
2025-05-06 17:19:49 +0700

    Update changelog

--- src/QueryCat.Tester/Program.cs
+++ src/QueryCat.Tester/Program.cs
@@ -81,20 +81,13 @@ public class Program
             RunBootstrapScript = true,
         };
 
-        var pluginLoaderFactory = (IExecutionThread thread) =>
-        {
-            var loader = new SimplePluginsAssemblyLoader(
-                workingDirectoryPlugins.Union(pluginDirectories),
-                thread.FunctionsManager);
-            // TODO: Remove after QueryCat update, this is a workaround due to bug in 0.12.1.
-            loader.LoadAsync(new PluginsLoadingOptions()).GetAwaiter().GetResult();
-            return loader;
-        };
         var executionThread = new ExecutionThreadBootstrapper(options)
             .WithStandardFunctions()
             .WithStandardUriResolvers()
             .WithConfigStorage(new MemoryInputConfigStorage())
-            .WithPluginsLoader(pluginLoaderFactory)
+            .WithPluginsLoader(thread => new SimplePluginsAssemblyLoader(
+                workingDirectoryPlugins.Union(pluginDirectories),
+                thread.FunctionsManager))
             .Create();
         await executionThread.PluginsManager.PluginsLoader.LoadAsync(new PluginsLoadingOptions(), CancellationToken.None);
 
