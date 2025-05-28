57280419a3fe23d13da4c316f492c070927c2f06
Ivan Kozhin <ivan@saritasa.com>
2025-05-28 20:42:30 +0700

    Add lock to ThriftClientLogger

--- sdk/dotnet-client/Logging/ThriftClientLogger.cs
+++ sdk/dotnet-client/Logging/ThriftClientLogger.cs
@@ -8,18 +8,24 @@ internal sealed class ThriftClientLogger : ILogger
 {
     private readonly string _name;
     private readonly ThriftPluginClient _client;
+    private readonly object _objLock = new();
+    private readonly LogLevel _minLogLevel = LogLevel.Trace;
 
     public ThriftClientLogger(string name, ThriftPluginClient client)
     {
         _name = name;
         _client = client;
+        if (_client.RegistrationResult != null)
+        {
+            _minLogLevel = SdkConvert.Convert(_client.RegistrationResult.MinLogLevel);
+        }
     }
 
     /// <inheritdoc />
     public IDisposable? BeginScope<TState>(TState state) where TState : notnull => null;
 
     /// <inheritdoc />
-    public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None;
+    public bool IsEnabled(LogLevel logLevel) => logLevel != LogLevel.None && _minLogLevel <= logLevel;
 
     /// <inheritdoc />
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
@@ -34,10 +40,13 @@ internal sealed class ThriftClientLogger : ILogger
         }
 
         var message = string.Concat(_name, ": ", formatter.Invoke(state, exception));
-        AsyncUtils.RunSync(ct => _client.LogAsync(
-            level: SdkConvert.Convert(logLevel),
-            message: message,
-            cancellationToken: ct)
-        );
+        lock (_objLock)
+        {
+            AsyncUtils.RunSync(ct => _client.LogAsync(
+                level: SdkConvert.Convert(logLevel),
+                message: message,
+                cancellationToken: ct)
+            );
+        }
     }
 }
