3cd23d71be9c0758af963d1472912fc2c247965b
Ivan Kozhin <ivan@saritasa.com>
2025-06-23 12:39:38 +0700

    Use Column attribute for schema column name

--- src/QueryCat.Backend.Core/Fetch/ClassRowsFrameBuilderExtensions.cs
+++ src/QueryCat.Backend.Core/Fetch/ClassRowsFrameBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using QueryCat.Backend.Core.Types;
@@ -28,14 +29,20 @@ public static class ClassRowsFrameBuilderExtensions
         [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)] Type? type,
         out List<PropertyInfo> properties) where TClass : class
     {
-        var props = (type ?? typeof(TClass)).GetProperties().Where(p => p.CanRead);
-        properties = new List<PropertyInfo>();
+        var props = (type ?? typeof(TClass)).GetProperties().Where(p => p.CanRead).ToArray();
+        properties = new List<PropertyInfo>(capacity: props.Length);
         foreach (var propertyInfo in props)
         {
-            var description = propertyInfo.GetCustomAttributes<DescriptionAttribute>()
-                .Select(a => a.Description).FirstOrDefault();
+            var descriptionAttribute = propertyInfo.GetCustomAttribute(typeof(DescriptionAttribute), inherit: false)
+                as DescriptionAttribute;
+            var description = descriptionAttribute != null ? descriptionAttribute.Description : string.Empty;
             var dataType = Converter.ConvertFromSystem(propertyInfo.PropertyType);
-            builder.AddProperty(propertyInfo.Name, dataType,
+            var columnAttribute = propertyInfo.GetCustomAttribute(typeof(ColumnAttribute), inherit: false)
+                as ColumnAttribute;
+            var name = columnAttribute != null ? columnAttribute.Name : propertyInfo.Name;
+            builder.AddProperty(
+                name ?? propertyInfo.Name,
+                dataType,
                 obj => VariantValue.CreateFromObject(propertyInfo.GetValue(obj)), description);
             properties.Add(propertyInfo);
         }
