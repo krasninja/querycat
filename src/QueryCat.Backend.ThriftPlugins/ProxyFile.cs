08a9b7b6fbd16826777c0d1355da51857f8bd6bc
Ivan Kozhin <ivan@saritasa.com>
2026-02-02 14:18:40 +0700

    Increase ProxyLatestVersion

--- src/QueryCat.Backend.ThriftPlugins/ProxyFile.cs
+++ src/QueryCat.Backend.ThriftPlugins/ProxyFile.cs
@@ -7,7 +7,7 @@ namespace QueryCat.Backend.ThriftPlugins;
 /// </summary>
 public static class ProxyFile
 {
-    public const int ProxyLatestVersion = 13;
+    public const int ProxyLatestVersion = 14;
     private const string ProxyExecutable = "qcat-plugins-proxy";
 
     /// <summary>
